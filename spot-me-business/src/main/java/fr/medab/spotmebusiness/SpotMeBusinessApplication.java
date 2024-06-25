0279ac3642280e7d937ba58c8982879b123b3409
Mohamed ABDALLAH <medaby@outlook.fr>
2024-06-25 11:43:05 +0200

    rename package #1

--- spot-me-business/src/main/java/fr/medab/spotmebusiness/SpotMeBusinessApplication.java
+++ spot-me-business/src/main/java/fr/medab/spotmebusiness/SpotMeBusinessApplication.java
@@ -1,4 +1,4 @@
-package fr.medab.spot_me_business;
+package fr.medab.spotmebusiness;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
