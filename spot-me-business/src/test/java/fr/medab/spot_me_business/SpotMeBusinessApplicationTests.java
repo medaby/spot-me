eba077ea9cffe6beddf08e4a6b7450c0a5b454f8
Mohamed ABDALLAH <medaby@outlook.fr>
2024-06-25 11:00:14 +0200

    inital commit with business project #1

---
+++ spot-me-business/src/test/java/fr/medab/spot_me_business/SpotMeBusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.medab.spot_me_business;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotMeBusinessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
