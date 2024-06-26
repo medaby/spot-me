94bf2df3c5c6ed0fb8a92553b584b490d6ef0bf7
Mohamed ABDALLAH <medaby@outlook.fr>
2024-06-26 12:27:18 +0200

    Added DDL + DML scripts #1

---
+++ spot-me-db/Scripts/data.dml.sql
@@ -0,0 +1,6 @@
+DELETE FROM t_spots;
+
+INSERT INTO t_spots (spot_name, spot_lat, spot_lng) VALUES
+('Zone industrielle', 48.78424, 2.34454),
+('Tour Eiffel', 48.8584717, 2.2936171),
+('Musée du Louvre', 48.8606111, 2.3350637);
